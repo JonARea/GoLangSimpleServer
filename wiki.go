f74a01a8f3bd760480537b5bd387b8dfc76a5bf6
Jon Rea <jonarea@gmail.com>
2018-04-16 23:13:35 -0400

    added muxer

--- wiki.go
+++ wiki.go
@@ -1,16 +1,53 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, my name is %s", r.URL.Path[1:])
 }
 
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		fmt.Fprintf(w, "Whoops, there was an error")
+	}
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	var dir string
+	flag.StringVar(&dir, "dir", ".", "current directory")
+	flag.Parse()
+	router := mux.NewRouter()
+	router.HandleFunc("/view/{file}", viewHandler).Methods("GET")
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir(dir)))
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
